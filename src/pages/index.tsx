8f4b847666e8cbe2aae91b4a9f40f22f963eaf86
Patrick Paiva <patrickpaiva88@gmail.com>
2022-08-18 15:07:58 -0300

    Home projects section done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Header from '../components/Header/Header'
 import Hero from '../components/Hero/Hero'
-import { Container, Presentation, Researches, Team } from '../styles/pages/Home'
+import { Container, Presentation, Projects, Researches, Team } from '../styles/pages/Home'
 
 const Home: React.FC = () => {
   return (
@@ -58,6 +58,31 @@ const Home: React.FC = () => {
             </div>
           </div>
         </Researches>
+        <Projects>
+          <div className="content">
+            <h2>OUR <span>PROJECTS</span></h2>
+            <div className="newsBox">
+              <div className="news">
+                <img src="/images/news1.png" alt="News 1 Image" />
+                <h3>News Sample</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <a href="\">Know More</a>
+              </div>
+              <div className="news">
+                <img src="/images/news2.png" alt="News 2 Image" />
+                <h3>News Sample</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <a href="\">Know More</a>
+              </div>
+              <div className="news">
+                <img src="/images/news3.png" alt="News 3 Image" />
+                <h3>News Sample</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <a href="\">Know More</a>
+              </div>
+            </div>
+          </div>
+        </Projects>
       </main>
     </Container>
   )
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -259,3 +259,78 @@ export const Researches = styled.div`
 
   }
 `
+
+export const Projects = styled.div`
+  width: 100vw;
+  max-width: 100%;
+  height: 600px;
+  background-color: #676363;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  color: white;
+  font-weight: 300;
+
+  .content {
+    width: 100vw;
+    max-width: 1200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 100%;
+
+    h2 {
+      font-size: 3.2rem;
+      font-weight: 300;
+
+      span {
+        color: ${(props) => props.theme.colors.green};
+      }
+    }
+
+    .newsBox {
+      width: 100%;
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+
+      .news {
+        max-width: 300px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+
+        h3 {
+          margin-top: 8px;
+          font-weight: 300;
+        }
+
+        p {
+          margin-top: 16px;
+          font-size: 1.4rem;
+          line-height: 2.4rem;
+          text-align: justify;
+          text-justify: inter-word;
+        }
+
+        a {
+          margin-top: 12px;
+          background-color: transparent;
+          border: 1px solid #fff;
+          padding: 6px 12px;
+          text-decoration: none;
+          color: #fff;
+          font-weight: 300;
+          border-radius: 4px;
+          transition: background-color 0.4s ease-out;
+
+          &:hover {
+            background-color: ${(props) => props.theme.colors.red};
+          }
+        }
+      }
+    }
+  }
+`
