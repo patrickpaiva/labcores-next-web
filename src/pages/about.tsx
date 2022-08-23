ac795e1cf101b44a4bb1d0de18fae8ea3e1db7ed
Patrick Paiva <patrickpaiva88@gmail.com>
2022-08-23 10:50:23 -0300

    about page done

---
+++ src/pages/about.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Banner from '../components/Banner/Banner'
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
+import { Container } from '../styles/pages/About'
+
+const About: React.FC = () => {
+
+  return (
+    <Container>
+      <Header/>
+      <Banner title="About Us" bannerColor="#86B932" />
+      <main>
+        <div className="content">
+          <h2>Humans as <span>sensors</span>. Smarter beings for <span>better cities</span>.</h2>
+          <p>
+          <img src="/images/about-image.png" alt="Woman with her notebook"/>
+            More than entertainment or learning, through social media, we can identify unprecedented levels of citizen engagement and participation. This activity is called “participatory sensing” or “citizen sensing”. Moreover, sensors are embedded into our everyday environment, such as cars, micro-chipped animals, elevators, smart buildings, environmental monitors and others. Many people are now carrying some form of sensor-laden device - a mobile phone, a tablet, a fitness device - from which sensor readings can also be retrieved. In this case, people interact – direct and indirect – with sensors. This is called ‘human-in-the-loop sensing’, empowering the citizen sensing with new kind of data. In citizen sensing, a sensor is not necessarily a hardware sensor, but can be a virtual sensor or a human interpreting sensory data. This data explosion has resulted in the emerging topic of “Big Social Data”. Big Social Data refers to large data volumes that relate to people interactions (with other people or things) or describe their behaviors, needs, and patterns. This data generates a cycle of knowledge creation, which can be useful to resolve problems in urban centers. From this social interaction, we can create more accurate knowledge about the city and its dynamics to support monitoring, surveillance and decision-making tasks of various kinds. <br /> <br />
+
+            The Lab CORES conducts multidisciplinary research in understanding, simulation and supporting of social interactions through the use of Big Social Data, trying to solving real-world problems and promoting wellness in populations. <br /> <br />
+
+            The Lab CORES is organized broadly into working groups including:
+          </p>
+          <img className="diagram" src="/images/cores-areas.png" alt="LabCores Working Groups" />
+        </div>
+      </main>
+      <Footer/>
+    </Container>
+  )
+}
+
+export default About
---
+++ src/styles/pages/About.ts
@@ -0,0 +1,73 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  main {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .content {
+      padding: 0 16px;
+      max-width: 1200px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      text-align: justify;
+      line-height: 3.4rem;
+      font-size: 1.8rem;
+
+      h2 {
+        font-size: 2.8rem;
+        margin: 24px 0;
+        text-align: center;
+        padding: 0 12px;
+
+        span {
+          color: ${props => props.theme.colors.green};
+        }
+
+        span:last-child{
+          color: ${props => props.theme.colors.blue};
+        }
+      }
+
+
+      p {
+        margin-bottom: 18px;
+        text-align: justify;
+        img {
+          float: left;
+          margin-right: 16px;
+        }
+      }
+
+      .diagram {
+        max-width: 600px;
+        width: 80vw;
+        margin-bottom: 32px;
+      }
+    }
+  }
+
+  @media(max-width: 920px) {
+    main {
+      .content {
+        p {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          img {
+            max-width: 450px;
+            width: 90vw;
+            float: none;
+            margin-right: 0px;
+            margin-bottom: 24px;
+          }
+        }
+      }
+    }
+  }
+`
