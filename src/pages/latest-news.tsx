d5a9c82e7ca2130f8bbaec49f207aa358b4efdde
Patrick Paiva <patrickpaiva88@gmail.com>
2023-08-03 11:02:00 -0300

    cria layout para latestNews

--- src/pages/latest-news.tsx
+++ src/pages/latest-news.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Banner from '../components/Banner/Banner'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
-import { Container } from '../styles/pages/LatestNews'
+import { Container, Projects } from '../styles/pages/LatestNews'
 import theme from '../styles/theme'
 
 const LatestNews: React.FC = () => {
@@ -11,7 +11,49 @@ const LatestNews: React.FC = () => {
     <Container>
       <Header/>
       <Banner title="Latest News" bannerColor={theme.colors.green} />
-
+      <Projects>
+          <div className="content">
+            <h2>OUR <span>PROJECTS</span></h2>
+            <div className="newsBox">
+              <div className="news">
+                <img src="/images/news1.png" alt="News 1 Image" />
+                <h3>News Sample</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <a href="\">Know More</a>
+              </div>
+              <div className="news">
+                <img src="/images/news2.png" alt="News 2 Image" />
+                <h3>News Sample</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <a href="\">Know More</a>
+              </div>
+              <div className="news">
+                <img src="/images/news3.png" alt="News 3 Image" />
+                <h3>News Sample</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <a href="\">Know More</a>
+              </div>
+              <div className="news">
+                <img src="/images/news1.png" alt="News 1 Image" />
+                <h3>News Sample</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <a href="\">Know More</a>
+              </div>
+              <div className="news">
+                <img src="/images/news2.png" alt="News 2 Image" />
+                <h3>News Sample</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <a href="\">Know More</a>
+              </div>
+              <div className="news">
+                <img src="/images/news3.png" alt="News 3 Image" />
+                <h3>News Sample</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <a href="\">Know More</a>
+              </div>
+            </div>
+          </div>
+        </Projects>
       <Footer/>
     </Container>
   )
--- src/styles/pages/LatestNews.ts
+++ src/styles/pages/LatestNews.ts
@@ -4,3 +4,102 @@ export const Container = styled.div`
   width: 100vw;
   max-width: 100%;
 `
+
+export const Projects = styled.div`
+  width: 100vw;
+  max-width: 100%;
+  padding: 32px 0;
+  background-color: #676363;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  color: white;
+  font-weight: 300;
+
+  .content {
+    width: 100vw;
+    max-width: 1200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 100%;
+
+    h2 {
+      font-size: 3.2rem;
+      font-weight: 300;
+      margin-bottom: 18px;
+
+      span {
+        color: ${(props) => props.theme.colors.green};
+      }
+    }
+
+    .newsBox {
+      width: 100%;
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      justify-content: space-evenly;
+      gap: 16px 0;
+      align-items: center;
+
+      .news {
+        max-width: 300px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+
+        h3 {
+          margin-top: 8px;
+          font-weight: 300;
+        }
+
+        p {
+          margin-top: 16px;
+          font-size: 1.4rem;
+          line-height: 2.4rem;
+          text-align: justify;
+          text-justify: inter-word;
+        }
+
+        a {
+          margin-top: 12px;
+          background-color: transparent;
+          border: 1px solid #fff;
+          padding: 6px 12px;
+          text-decoration: none;
+          color: #fff;
+          font-weight: 300;
+          border-radius: 4px;
+          transition: background-color 0.4s ease-out;
+
+          &:hover {
+            background-color: ${(props) => props.theme.colors.red};
+          }
+        }
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    height: 100%;
+    padding: 32px 0;
+
+    .content {
+      .newsBox {
+        display: flex;
+        flex-direction: column;
+        margin-top: 24px;
+
+        .news {
+          margin-bottom: 32px;
+          align-items: center;
+          h3 {
+            font-weight: 600;
+          }
+        }
+      }
+    }
+  }
+`
